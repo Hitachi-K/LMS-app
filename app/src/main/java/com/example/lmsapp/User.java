25102d2e6ff380b67b9e08a4c12ab40003918735
Kalindu Kumarage <68530264+real-crypto@users.noreply.github.com>
2020-09-25 16:32:52 +0530

    LMS app second commit

---
+++ app/src/main/java/com/example/lmsapp/User.java
@@ -0,0 +1,62 @@
+package com.example.lmsapp;
+
+public class User {
+
+    String email;
+    String name;
+    String userName;
+    String password;
+    Long contact;
+    String type;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getContact() {
+        return contact;
+    }
+
+    public void setContact(Long contact) {
+        this.contact = contact;
+    }
+
+    public User() {}
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
